fad30a2c70e7a17e0634a1f2026abdd6050acf4b
Ziad Teama <ziadbasaria600@gmail.com>
2024-06-14 23:12:17 +0300

    Search page Works ig

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import axios from "axios";
 const app = express();
 const port = 3000;
-const searchApiUrl='https://openlibrary.org/search.json';
+const searchApiUrl = "https://openlibrary.org/search.json";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-let users=[
-    { id: 1, name: "Angela", color: "teal" },
-    { id: 2, name: "Jack", color: "powderblue" },
+let users = [
+  { id: 1, name: "Angela", color: "teal" },
+  { id: 2, name: "Jack", color: "powderblue" },
 ];
-app.get("/",(req,res)=>{
-    res.render("home.ejs",{users:users});
+app.get("/", (req, res) => {
+  res.render("home.ejs", { users: users });
 });
-app.get("/search",(req,res)=>{
-res.render("search.ejs",{bookId:14625765,
-    authorName:"Magdy Shata"
-});
+app.post("/search", async (req, res) => {
+  const searchTitle = req.body.searchTitle;
+  console.log(searchTitle);
+  const response = await axios.get(
+   searchApiUrl+`?title=${searchTitle}`
+  );
+  const booksFound = response.data;
+  console.log(booksFound.numsFound);
+  res.render("search.ejs", {
+    booksFound: booksFound.docs,
+    numFound: booksFound.numFound,
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  console.log(`Server running on port ${port}`);
+});
