85d107393f67c560935a3815fb44e1ffcbeef667
Ziad Teama <ziadbasaria600@gmail.com>
2024-06-14 19:50:27 +0300

    nazel awady assser eltamreeen

--- app.js
+++ app.js
@@ -3,7 +3,24 @@ import bodyParser from "body-parser";
 import pg from "pg";
 const app = express();
 const port = 3000;
+const searchApiUrl='https://openlibrary.org/search.json';
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+
+let users=[
+    { id: 1, name: "Angela", color: "teal" },
+    { id: 2, name: "Jack", color: "powderblue" },
+];
+app.get("/",(req,res)=>{
+    res.render("home.ejs",{users:users});
+});
+app.get("/search",(req,res)=>{
+res.render("search.ejs",{bookId:14625765,
+    authorName:"Magdy Shata"
+});
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
