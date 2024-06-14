bc829173e559ede26e0cb6e8080f98e5a3003289
Ziad Teama <ziadbasaria600@gmail.com>
2024-06-15 01:20:05 +0300

    my books section left

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import axios from "axios";
+import { config } from "process";
 const app = express();
 const port = 3000;
 const searchApiUrl = "https://openlibrary.org/search.json";
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "book",
+    password: "Ziadteama1",
+    port: 5432,
+  });
+  db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -16,6 +25,13 @@ let users = [
 app.get("/", (req, res) => {
   res.render("home.ejs", { users: users });
 });
+app.get("/mybooks",async (req,res)=>{
+    const result = await db.query('SELECT * FROM public.books');
+    const myBooks= result.rows;
+    
+    res.render
+});
+
 app.post("/search", async (req, res) => {
   const searchTitle = req.body.searchTitle;
   console.log(searchTitle);
@@ -23,11 +39,21 @@ app.post("/search", async (req, res) => {
    searchApiUrl+`?title=${searchTitle}`
   );
   const booksFound = response.data;
-  console.log(booksFound.numsFound);
   res.render("search.ejs", {
     booksFound: booksFound.docs,
     numFound: booksFound.numFound,
   });
+});
+app.post("/add", async (req,res)=>{
+    const authorName = req.body.authorName;
+    const publishYear = req.body.PublishYear;
+    const bookTitle = req.body.bookTitle;
+    const bookId = req.body.bookId;
+
+    const result = await db.query(`INSERT INTO public.books(
+	 title, author, publishyear, coverid)
+	VALUES ($1, $2, $3, $4);`,[bookTitle,authorName,publishYear,bookId]);
+    res.redirect("/search");
 });
 
 app.listen(port, () => {
