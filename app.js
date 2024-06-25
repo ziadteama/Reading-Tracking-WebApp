3c41515e02c90f8b5034dbace8c8f7f743c5d5eb
Ziad Teama <ziadbasaria600@gmail.com>
2024-06-25 03:12:53 +0300

    google oauth added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import bcrypt from "bcrypt";
 import passport from "passport";
 import session from "express-session";
 import { Strategy } from "passport-local";
+import GoogleStrategy from "passport-google-oauth2";
 import env from "dotenv";
 
 const app = express();
@@ -86,6 +87,20 @@ app.get("/", async (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"],
+  })
+);
+
+app.get(
+  "/auth/google/books",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
 
 app.get("/logout", (req, res) => {
   req.logout(function (err) {
@@ -252,6 +267,35 @@ passport.use(
     }
   })
 );
+
+passport.use("google",new GoogleStrategy(
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/books",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+  },
+  async (accestoken, refreshtoken, profile, cb) => {
+    console.log(profile.displayName);
+    try {
+      console.log(profile);
+      const result=await db.query("SELECT * from users WHERE email=$1",[
+        profile.email
+      ]);
+      if(result.rows.length>0)
+        return cb(null,result.rows[0]);
+      else{
+        const newUser = await db.query(
+          "INSERT INTO users (email, name, password) VALUES ($1, $2,$3)",
+          [profile.email, profile.displayName, "google"]
+        );
+        return cb(null,newUser.rows[0]);
+      }
+    } catch (error) {
+      return cb(error);
+    }
+  }
+));
 
 passport.serializeUser((user, cb) => {
   cb(null, user);
